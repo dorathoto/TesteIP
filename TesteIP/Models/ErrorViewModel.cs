bf71b83142bb7e9d3ca1c4e30c23d04d427c28f9
Alec_Gracelli <alecgracelli@gmail.com>
2022-10-14 15:34:28 -0300

    Add project files.

---
+++ TesteIP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
